50902f0086eb22e2be118ea7500b81e46174c6ab
DenissonLins <45289850+DenissonLins@users.noreply.github.com>
2018-11-23 23:57:05 -0200

    Update Mexico.java

--- src/territorios/america_do_norte/Mexico.java
+++ src/territorios/america_do_norte/Mexico.java
@@ -1,4 +1,42 @@
 
-public class Mexico {
+package territorios.america_do_norte;
 
-}
+import territorios.*;
+
+public final class Mexico extends territorios {
+	
+
+	    public final static Mexico instance;
+	    
+	    static {
+	        instance = new Mexico();
+	    }
+
+	    private Mexico() {}
+
+	    public static Mexico getInstance() {
+	        return instance;
+	    }
+	    
+	    @Override
+	    public Territorio[] getVizinhos() {
+	        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	    }
+
+	    @Override
+	    public Territorio getTerritorio() {
+	        return Mexico.getInstance();
+	    }
+
+	    @Override
+	    public Tag setTag() {
+	        return Tag.MEXICO;
+	    }
+	    
+	    @Override
+	    public String toString() {
+	        return "México";
+	    }
+	    
+	}
+
